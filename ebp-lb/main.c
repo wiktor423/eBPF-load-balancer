7ee643148c19bff90dde99cc0836aa03d502cbff
Piotr Łądkowski <piolad2003@gmail.com>
2026-09-08 23:31:48 +0200

    feat: add NATing

--- ebp-lb/main.c
+++ ebp-lb/main.c
@@ -3,12 +3,16 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
+#define IP_MF       0x2000
+#define IP_OFFSET   0x1FFF
 
 #define MAX_PATH 256
 /*u8[6]*/
-#define DEST_MAC_ADDRESS [0xbc, 0x24, 0x11, 0x70, 0x42, 0xc4]
+#define DEST_MAC_ADDRESS {0xbc, 0x24, 0x11, 0x70, 0x42, 0xc4}
+#define LB_MAC_ADDRESS   {0xbc, 0x24, 0x11, 0x10, 0x30, 0x16}
 /*u32*/   
 #define DEST_IP_ADDRESS 3232235842 
+#define LB_IP_ADDRESS  3232235846 
 /*u16*/ 
 #define BACKEND_PORT 4321
 
@@ -21,9 +25,64 @@ struct {
 } udp_pkt_cnt_per_port SEC(".maps");
 
 
-SEC("xdp")
-int load_balance(struct xdp_md* ctx) {
+static __always_inline __u16 csum_fold(__u32 csum) {
+    csum = (csum & 0xffff) + (csum >> 16);
+    csum = (csum & 0xffff) + (csum >> 16);
+    return ~csum;
+}
+
+// recalculate IPv4 checksum in place
+static __always_inline int rewrite_ipv4_csum(struct iphdr *iph) {
+    __s64 csum;
+    iph->check = 0;
+
+    // calcualte checksum anew using bpf helper 
+    // - ip header is small and we change almost all fields
+    csum = bpf_csum_diff(0,0, (__be32 *) iph, sizeof(*iph),0);
+    if(csum <0)
+      return -1;
+    iph->check = csum_fold((__u32) csum);
+
+    return 0;
+}
+
+// recaluclate UDP checksum using previously known checksum
+static __always_inline int rewrite_udp_checksum(
+    struct udphdr *udp, __be32 old_saddr, __be32 new_saddr,
+    __be32 old_daddr,  __be32 new_daddr,__u32 old_ports,
+    __u32 new_ports ){
+
+    __s64 csum;
+
+    if(udp->check == 0) return 0;
+
+    // inverse current checksum
+    csum = ~((__u32) udp->check);
+
+    // diff of source address
+    csum = bpf_csum_diff(&old_saddr, sizeof(old_saddr), &new_saddr, sizeof(new_saddr), (__u32) csum);
+    // diff of destination address
+    csum = bpf_csum_diff(&old_daddr, sizeof(old_daddr), &new_daddr, sizeof(new_daddr), (__u32) csum);
+    // diff of ports combined for efficient use of 32 bits of diff function
+    csum = bpf_csum_diff((__be32 *) &old_ports, sizeof(old_ports), (__be32 *) &new_ports, sizeof(new_ports), (__u32) csum);
+
+    if(csum < 0)
+      return -1;
+
+    udp-> check = csum_fold((__u32) csum);
     
+
+    // RFC768 requires 0 to be transmited as all ones
+    if (udp->check == 0)
+      udp->check=0xffff;
+
+    return 0;
+}
+
+
+
+SEC("xdp")
+int load_balance(struct xdp_md* ctx) {  
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
@@ -32,6 +91,14 @@ int load_balance(struct xdp_md* ctx) {
     
     struct iphdr *ip = data + sizeof(*eth);
     if ((void*)ip + sizeof(*ip) <= data_end) {
+
+      if(ip->ihl != 5)
+        return XDP_PASS; // todo: no support for IPv4 options
+      
+      
+      if(ip->frag_off & bpf_htons(IP_MF | IP_OFFSET))
+        return XDP_PASS; //todo: no suport for fragments
+      
   
       if (ip->protocol == IPPROTO_UDP) {  
         struct udphdr *udp = (void*)ip + sizeof(*ip);
@@ -43,14 +110,41 @@ int load_balance(struct xdp_md* ctx) {
               return XDP_PASS; 
             }
 
-            u8 dest_mac[] = DEST_MAC_ADDRESS; 
+            if (ip->ttl <= 1)
+              return XDP_DROP;
 
-            for(int i=0; i<6; i++){
-              eth->dest[i] = dest_mac[i];
-            }
+            __be32 old_saddr = ip->saddr;
+            __be32 old_daddr = ip->daddr;
+
+            __u32 old_ports;
+            __builtin_memcpy(&old_ports,&udp->source,sizeof(old_ports));
+
+            __be32 new_saddr = LB_IP_ADDRESS; 
+            __be32 new_daddr = bpf_htonl(DEST_IP_ADDRESS);
+
+            __be16 new_sport = bpf_htons(6868);
+            __be16 new_dport = bpf_htons(BACKEND_PORT);
+
+            ip->ttl--;
+
+            __u32 new_ports;
+            __builtin_memcpy(&new_ports, &udp->source, sizeof(new_ports));
+
+
             
-            ip->dest = htonl(DEST_IP_ADDRESS)
-            udp->dest = htons(BACKEND_PORT);
+            if (rewrite_udp_checksum(udp, old_saddr, new_saddr, old_daddr, new_daddr, old_ports, new_ports) < 0)
+            return XDP_ABORTED;
+            if (rewrite_ipv4_csum(ip) < 0)
+            return XDP_ABORTED;
+            
+
+            u8 dest_mac[] = DEST_MAC_ADDRESS; 
+            for(int i=0; i<6; i++){
+              (eth->h_dest)[i] = dest_mac[i];
+            }            
+
+            
+
             return XDP_TX;
             // __u64 val = 1;
 
